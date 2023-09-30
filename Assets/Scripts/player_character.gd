ac99c4d41ca28e246b3bf50cedf6373fe718f92d
Adam Akkari <theadamsmccall@gmail.com>
2023-10-01 01:43:35 +0200

    added player dash with hud

--- Assets/Scripts/player_character.gd
+++ Assets/Scripts/player_character.gd
@@ -1,19 +1,29 @@
 extends CharacterBody3D
 
 @export var speed = 10
+@export var dash_speed = 35
 @export var jump_velocity = 4.5
 @export var grid:GridGenerator
 
 var mouse_sensitivity = ProjectSettings.get_setting("player/mouse_sensitivity")
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var horizontal_velocity = Vector2.ZERO
 var vertical_velocity = 0
+var dash_cooldown_value = 4
+var is_dashing = false
 var is_dead = false
 var dead_cam_set = false
 
 @onready var camera:Camera3D = $Camera3D
 @onready var shoot_cooldown:Timer = $shoot_cooldown
+@onready var dash_timer:Timer = $dash_timer
+@onready var dash_cooldown:Timer = $dash_cooldown_part
 @onready var crosshair_enabled_sprite = $HUD/CenterContainer/crosshair_enabled
 @onready var crosshair_disabled_sprite = $HUD/CenterContainer/crosshair_disabled
+@onready var dash_cooldown_sprite_1 = $HUD/CenterContainer/dash_cooldown_tl
+@onready var dash_cooldown_sprite_2 = $HUD/CenterContainer/dash_cooldown_tr
+@onready var dash_cooldown_sprite_3 = $HUD/CenterContainer/dash_cooldown_br
+@onready var dash_cooldown_sprite_4 = $HUD/CenterContainer/dash_cooldown_bl
 @onready var collider:CollisionShape3D = $CollisionShape3D
 
 func _ready():
@@ -43,8 +53,20 @@ func _input(event):
 
 func handle_movement(delta):
 	# Horizontal movement
-	var horizontal_velocity = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").normalized() * speed
+	if !is_dashing:
+		horizontal_velocity = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").normalized() * speed
+	else:
+		horizontal_velocity = horizontal_velocity.normalized() * dash_speed
 	velocity = horizontal_velocity.x * global_transform.basis.x + horizontal_velocity.y * global_transform.basis.z
+	
+	if horizontal_velocity != Vector2.ZERO and Input.is_action_just_pressed("dash") and can_dash():
+		is_dashing = true
+		dash_timer.start()
+		dash_cooldown_value = 0
+		dash_cooldown_sprite_1.visible = false
+		dash_cooldown_sprite_2.visible = false
+		dash_cooldown_sprite_3.visible = false
+		dash_cooldown_sprite_4.visible = false
 	
 	# Jump & gravity
 	if is_on_floor():
@@ -88,3 +110,25 @@ func killed():
 	crosshair_enabled_sprite.visible = false
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	collider.disabled = true
+
+func _on_dash_timer_timeout():
+	is_dashing = false
+	dash_cooldown.start()
+
+func _on_dash_cooldown_timeout():
+	dash_cooldown_value += 1
+	if dash_cooldown_value == 1:
+		dash_cooldown_sprite_1.visible = true
+		dash_cooldown.start()
+	if dash_cooldown_value == 2:
+		dash_cooldown_sprite_2.visible = true
+		dash_cooldown.start()
+	if dash_cooldown_value == 3:
+		dash_cooldown_sprite_3.visible = true
+		dash_cooldown.start()
+	if dash_cooldown_value == 4:
+		dash_cooldown_sprite_4.visible = true
+		dash_cooldown.start()
+
+func can_dash():
+	return dash_cooldown_value >= 4
