9969fd852c38f32eea2ee204bfdfebf0c698bdd1
Adam Akkari <theadamsmccall@gmail.com>
2023-10-01 20:43:36 +0200

    Fixed god forsaken UI

--- Assets/Scripts/QuitButton.gd
+++ Assets/Scripts/QuitButton.gd
@@ -1,17 +1,14 @@
 extends Area2D
 
 @onready var sprite = $Sprite2D
-var base_pos = Vector2.ZERO
+var base_sprite_pos = Vector2.ZERO
 var is_selected = false
-
-func _ready():
-	base_pos = sprite.position
 
 func _process(delta):
 	if is_selected:
-		sprite.position = sprite.position.lerp(base_pos + (Vector2.RIGHT * 25), 10 * delta)
+		sprite.position = sprite.position.lerp(base_sprite_pos + (Vector2.RIGHT * 25), 10 * delta)
 	else:
-		sprite.position = sprite.position.lerp(base_pos, 10 * delta)
+		sprite.position = sprite.position.lerp(base_sprite_pos, 10 * delta)
 
 func _on_mouse_entered():
 	is_selected = true
